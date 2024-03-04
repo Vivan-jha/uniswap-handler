adba3971f5289f0f5031ea95eefce0b8e0e19f31
TusharTapadia <tushartapadia12@gmail.com>
2024-03-05 01:48:42 +0530

    Added changes to server to make it compatible

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 const ethers = require("ethers");
 var express = require("express");
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 require('dotenv').config()
 
 const qs = require("qs");
@@ -21,145 +23,99 @@ const provider = new ethers.providers.JsonRpcProvider(
   }
 );
 
-const privateKey = 'bf104dcce9d28bc2280701749d2d7dd184ad49695b8b9f38e1e924ec8482640c';
-const wallet = new ethers.Wallet(privateKey, provider); 
-const signer = wallet.provider.getSigner(wallet.address);
-
-
-
-app.get("/bestRates", async function (req,res){
+app.post("/bestRates", async function (req,res){
     try{
-        const r = await getSwapData(req.query.sellTokenAddress,req.query.buyTokenAddress,req.query.sellTokenAmount)
+        const r = await getSwapData(req.body.sellTokenAddress,req.body.buyTokenAddress,req.body.sellTokenAmount) 
         res.send(r);
     }catch(e){
         console.log(e);
         res.status(500);
     }
 })
-
-async function getRatesArray(sellTokenArray, buyTokenArray, sellAmountArray){
-  try {
-    if (!(sellTokenArray.length === buyTokenArray.length && sellTokenArray.length === sellAmountArray.length)) {
-      throw new Error("Input arrays must have the same length.");
-    }
-    let rateResults = [];
-
-    for (let i = 0; i < sellTokenArray.length; i++) {
-      let rateResult = await getSwapData(sellTokenArray[i], buyTokenArray[i], sellAmountArray[i]);
-      if(rateResult !== null) {
-        rateResults.push(rateResult);
-      } else {
-        console.log(`Failed to get swap data for pair index ${i}`);
-        rateResults.push(null);
-      }
-    }
-    
-    return rateResults;
-  } catch (e) {
-    console.error("An error occurred in getRatesArray:", e);
-  }
-}
-
-async function approveToken(contractAddress, spender, amount) {
-  try {
-    const tokenContract = new ethers.Contract(contractAddress, erc20abi.abi, signer);
-
-
-    const calldata = tokenContract.interface.encodeFunctionData("approve", [spender, amount]);
-    return calldata;
-  } catch (error) {
-    console.error("Approval Error:", error);
-    throw error;
-  }
-}
-
-function delay(ms) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
-}
-
-
-// async function getSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount){
-//     const zeroExData = await getZeroExSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount);
-//     const oneInchData = await getOneInchSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount);
-
-//     if(zeroExData.grossBuyAmount == null){
-//       if(oneInchData.toAmount == null){
-//         console.log("Error in fetching rates on both 0x and 1inch");
-//         return null;
-//       }else{
-//         let approvaldata=approveToken(sellTokenAddress,ONEINCH_ROUTER_ADDRESS,sellTokenAmount);
-//         let temp = {
-//           sellTokenAddress: oneInchData.tx.from,
-//           buyTokenAddress: oneInchData.tx.to,
-//           sellTokenAmount: oneInchData.toAmount,
-//           calldata: [approvaldata,oneInchData.tx.data],
-//           protocol: "1Inch"
-//         }
-//         return(temp);
-//       }
-//     }else if(oneInchData.toAmount == null){
-//       if(zeroExData.grossBuyAmount == null){
-//         console.log("Error in fetching rates on both 0x and 1inch");
-//         return null;
-//       }else{
-//         approvaldata=approveToken(sellTokenAddress,ZEROEX_ROUTER_ADDRESS,sellTokenAmount);
-//         let temp = {
-//           sellTokenAddress: zeroExData.sellTokenAddress,
-//           buyTokenAddress: zeroExData.buyTokenAddress,
-//           sellTokenAmount: zeroExData.grossBuyAmount,
-//           calldata: [approvaldata,zeroExData.data],
-//           protocol: "ZeroEx"
-//         }
-//         return(temp);
-//       }
-//     }else{
-//       if(zeroExData.grossBuyAmount >= oneInchData.tx.toAmount){
-//         approvaldata=approveToken(sellTokenAddress,ZEROEX_ROUTER_ADDRESS,sellTokenAmount);
-//         let temp = {
-//           sellTokenAddress: zeroExData.sellTokenAddress,
-//           buyTokenAddress: zeroExData.buyTokenAddress,
-//           sellTokenAmount: zeroExData.grossBuyAmount,
-//           calldata:[approvaldata,zeroExData.data],
-//           protocol: "ZeroEx"
-//         }
-//         return(temp);
-//       }else{
-//         let approvaldata=approveToken(sellTokenAddress,ONEINCH_ROUTER_ADDRESS,sellTokenAmount);
-//         let temp = {
-//           sellTokenAddress: oneInchData.tx.from,
-//           buyTokenAddress: oneInchData.tx.to,
-//           sellTokenAmount: oneInchData.toAmount,
-//           calldata: [approvaldata,oneInchData.tx.data],
-//           protocol: "1Inch"
-//         }
-//         return(temp);
-//       }
-//     }
-// }
 async function getSwapData(sellTokenAddress, buyTokenAddress, sellTokenAmount) {
-  const zeroExData = await getZeroExSwapData(sellTokenAddress, buyTokenAddress, sellTokenAmount);
-
-  if (zeroExData.grossBuyAmount == null) {
-    console.log("Error in fetching rates on ZeroEx");
-    return null;
-  } else {
-    // Corrected by adding 'await' to ensure 'approvaldata' resolves before proceeding
-    let approvaldata = await approveToken(sellTokenAddress, ZEROEX_ROUTER_ADDRESS, sellTokenAmount);
-
-
-    let temp = {
-      sellTokenAddress: zeroExData.sellTokenAddress,
-      buyTokenAddress: zeroExData.buyTokenAddress,
-      sellTokenAmount: zeroExData.grossBuyAmount,
-      calldata: [approvaldata, zeroExData.data], // 'approvaldata' should now be the resolved value
-      protocol: "ZeroEx"
-    };
-
-    return temp;
+  if ((sellTokenAddress.length != buyTokenAddress.length || sellTokenAddress.length != sellTokenAmount.length)) {
+    throw new Error("Input arrays must have the same length.");
   }
+  const responseArray = [];
+  for(let i =0;i<sellTokenAddress.length;i++){
+    console.log(sellTokenAddress[i],buyTokenAddress[i],sellTokenAmount[i])
+    responseArray.push(await getSwapDataInternal(sellTokenAddress[i],buyTokenAddress[i],sellTokenAmount[i]));
+  }
+  return responseArray;
 }
+async function getSwapDataInternal(sellTokenAddress,buyTokenAddress,sellTokenAmount){
+  try{
+  const zeroExData = await getZeroExSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount);
+  const oneInchData = await getOneInchSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount);
 
- 
+  console.log(zeroExData);
+  console.log(oneInchData)
+
+  if(zeroExData.grossBuyAmount == null){
+    if(oneInchData.toAmount == null){
+      console.log("Error in fetching rates on both 0x and 1inch");
+      return null;
+    }else{
+      let approvaldata=await approveToken(sellTokenAddress,ONEINCH_ROUTER_ADDRESS,sellTokenAmount);
+      let temp = {
+        sellTokenAddress: oneInchData.tx.from,
+        sellTokenAmount: sellTokenAmount,
+        buyTokenAddress: oneInchData.tx.to,
+        buyTokenAmount: oneInchData.toAmount,
+        calldata: [approvaldata,oneInchData.tx.data],
+        gas: oneInchData.tx.gas,
+        protocol: "1Inch"
+      }
+      return(temp);
+    }
+  }else if(oneInchData.toAmount == null){
+    if(zeroExData.grossBuyAmount == null){
+      console.log("Error in fetching rates on both 0x and 1inch");
+      return null;
+    }else{
+      approvaldata=await approveToken(sellTokenAddress,ZEROEX_ROUTER_ADDRESS,sellTokenAmount);
+      let temp = {
+        sellTokenAddress: zeroExData.sellTokenAddress,
+        sellTokenAmount: sellTokenAmount,
+        buyTokenAddress: zeroExData.buyTokenAddress,
+        buyTokenAmount: zeroExData.grossBuyAmount,
+        calldata: [approvaldata,zeroExData.data],
+        gas: zeroExData.estimatedGas,
+        protocol: "ZeroEx"
+      }
+      return(temp);
+    }
+  }else{
+    if(zeroExData.grossBuyAmount >= oneInchData.tx.toAmount){
+      approvaldata=await approveToken(sellTokenAddress,ZEROEX_ROUTER_ADDRESS,sellTokenAmount);
+      let temp = {
+        sellTokenAddress: zeroExData.sellTokenAddress,
+        sellTokenAmount: sellTokenAmount,
+        buyTokenAddress: zeroExData.buyTokenAddress,
+        buyTokenAmount: zeroExData.grossBuyAmount,
+        calldata:[approvaldata,zeroExData.data],
+        gas: zeroExData.estimatedGas,
+        protocol: "ZeroEx"
+      }
+      return(temp);
+    }else{
+      let approvaldata=await approveToken(sellTokenAddress,ONEINCH_ROUTER_ADDRESS,sellTokenAmount);
+      let temp = {
+        sellTokenAddress: oneInchData.tx.from,
+        sellTokenAmount: sellTokenAmount,
+        buyTokenAddress: oneInchData.tx.to,
+        buyTokenAmount: oneInchData.toAmount,
+        calldata: [approvaldata,oneInchData.tx.data],
+        gas: oneInchData.tx.gas,
+        protocol: "1Inch"
+      }
+      return(temp);
+    }
+  }
+}catch(e){
+  console.log("ERROR IN MAKING DATA", e);
+}
+}
 async function getZeroExSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount){
   try{
     const params = {
@@ -192,10 +148,10 @@ async function getOneInchSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmou
       from: "0xbEbEbEb035351f58602E0C1C8B59ECBfF5d5f47b",
       slippage: 0.01,
       disableEstimate: true,
-      skipValidation: true
+      includeGas: true
       }
       const response = await axios.get(
-        `https://api.1inch.dev/swap/v5.2/56/swap?${qs.stringify(params)}`,
+        `https://api.1inch.dev/swap/v6.0/56/swap?${qs.stringify(params)}`,
         {
           headers: {
             'Authorization': `Bearer ${process.env.ONEINCH_API_KEY}`,
@@ -207,73 +163,19 @@ async function getOneInchSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmou
     console.log("0x Error",e);
 }
 }
-async function getParaSwapData(sellTokenAddress,buyTokenAddress,sellTokenAmount){
-  try{
-    const params = {
-      srcToken: sellTokenAddress,
-      destToken: buyTokenAddress,
-      amount: sellTokenAmount,
-      network:56,
-      srcDecimals:18,
-      destDecimals:6,
-      side:'SELL'
-      }
-      const response = await axios.get(
-        `https://apiv5.paraswap.io/prices/?${qs.stringify(params)}`
-      )
-      return(response.data);
- }catch(e){
-    console.log("Paraswap Error",e);
- }
- }
- 
- 
- async function buildParaSwapTransaction({
-  network,
-  userAddress,
-  priceRoute,
-  srcToken,
-  destToken,
-  srcAmount,
-  destAmount,
-  slippage,
-  gasPrice,
-  eip1559,
-  ignoreChecks,
-  ignoreGasEstimate,
- }) {
-  const transactionParams = {
-    network,
-    userAddress,
-    priceRoute,
-    srcToken,
-    destToken,
-    srcAmount,
-    destAmount,
-    slippage,
-    gasPrice,
-    eip1559,
-    ignoreChecks,
-    ignoreGasEstimate,
-  };
- 
- 
+async function approveToken(contractAddress, spender, amount) {
   try {
-    const txResponse = await axios.post(
-      `https://apiv5.paraswap.io/transactions/${network}`,
-      transactionParams
-    );
-    return txResponse.data;
+    const tokenContract = new ethers.Contract(contractAddress, erc20abi.abi, provider);
+    const calldata = tokenContract.interface.encodeFunctionData("approve", [spender, amount]);
+    return calldata;
   } catch (error) {
-    console.error('Transaction Build Error:', error);
+    console.error("Approval Error:", error);
     throw error;
   }
- }
-
-
- 
- 
-
+}
+function delay(ms) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
